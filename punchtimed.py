5e92b57b1382b76636cbf4abe26550c9e27c7560
Tim Johnson <tcjohnso@stanford.edu>
2015-03-20 10:04:21 -0700

    Writes PID file to /tmp

--- punchtimed.py
+++ punchtimed.py
@@ -4,13 +4,30 @@
 # Listens for signal and punches a user in when signal received
 # Signal used: SIGUSR2
 
+import posix
 import signal
+import time
 
 def usr2_received(signal, frame):
 	print "Signal received"
 	
 
 if __name__ == '__main__':
-	signal.signal(signal.SIGUSR2, usr2_received)
-	signal.pause()
+	# TODO: don't run again if pid file exists
 	
+	my_pid = posix.getpid()
+	try:
+		pid_file = file('/tmp/punchtime.pid','w')
+	except: 
+		print "couldn't open pid file.  aborting"
+		exit(1)
+		
+	pid_file.write(str(my_pid))
+	pid_file.close()
+	
+	done = False
+	signal.signal(signal.SIGUSR2, usr2_received)
+	while not done:
+		signal.pause()
+	
+	# cleanup: remove PID file
